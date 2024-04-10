54f70c50bb537597a458020518ad081518815080
yash-axonator <161823038+yash-axonator@users.noreply.github.com>
2024-04-10 11:48:13 +0530

    Update myscript.sh

--- selenium_tests/myscript.sh
+++ selenium_tests/myscript.sh
@@ -1,2 +1,3 @@
-dotnet test  /var/lib/jenkins/workspace/web_automation/selenium_tests/bin/Debug/net8.0/selenium_tests.dll --logger html
+cd ./web_automation/selenium_tests/bin/Debug/net8.0
+dotnet test  selenium_tests.dll --logger html
 

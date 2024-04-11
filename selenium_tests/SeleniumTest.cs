9b073d692e935f4743b2f7f10105dc8187ba73dc
yash-axonator <161823038+yash-axonator@users.noreply.github.com>
2024-04-11 11:28:45 +0530

    Update SeleniumTest.cs

--- selenium_tests/SeleniumTest.cs
+++ selenium_tests/SeleniumTest.cs
@@ -197,7 +197,7 @@ public class SeleniumTest
 
     try
     {
-    testLogger.GenerateHtmlReport("/home/l21/Documents/qa_automation/selenium_tests/bin/Debug/net8.0/TestReport.html");
+    testLogger.GenerateHtmlReport("TestReport.html");
     }
     catch (UnauthorizedAccessException ex)
     {

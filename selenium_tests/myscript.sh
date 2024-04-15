a89f39bdaa0eb759d65a80787ee2b954a48b809d
yash-axonator <161823038+yash-axonator@users.noreply.github.com>
2024-04-15 14:28:11 +0530

    Update myscript.sh

--- selenium_tests/myscript.sh
+++ selenium_tests/myscript.sh
@@ -1,8 +1,2 @@
-# Give read and write permissions to the Jenkins user for the directory
-sudo chmod -R u+rw /home/l21/Documents/qa_automation/selenium_tests/bin/Debug/net8.0/
-
-# Give read and write permissions to the Jenkins user for the file
-sudo chmod u+rw /home/l21/Documents/qa_automation/selenium_tests/bin/Debug/net8.0/TestReport.html
-
-(which dotnet) test  /var/lib/jenkins/workspace/web_automation/selenium_tests/bin/Debug/net8.0/selenium_tests.dll --logger html
+(which dotnet) test  /var/lib/jenkins/workspace/Web_Automation/selenium_tests/bin/Debug/net8.0/selenium_tests.dll --logger html
 

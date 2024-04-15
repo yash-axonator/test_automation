3eb24e5345e16cad0b8903303f18911c617453bd
yash-axonator <161823038+yash-axonator@users.noreply.github.com>
2024-04-15 16:17:03 +0530

    Update myscript.sh

--- selenium_tests/myscript.sh
+++ selenium_tests/myscript.sh
@@ -1,3 +1,3 @@
-apt-get update
-apt-get install -y dotnet-sdk-8.0
-dotnet test  /var/lib/jenkins/workspace/Web_Automation/selenium_tests/bin/Debug/net8.0/selenium_tests.dll --logger html
+sudo apt-get update
+sudo apt-get install -y dotnet-sdk-8.0
+sudo dotnet test  /var/lib/jenkins/workspace/Web_Automation/selenium_tests/bin/Debug/net8.0/selenium_tests.dll --logger html

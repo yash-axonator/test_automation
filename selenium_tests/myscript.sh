3edf74105d68b8359e02beee70ac96dea3d0d2e2
yash-axonator <161823038+yash-axonator@users.noreply.github.com>
2024-04-15 15:18:21 +0530

    Update myscript.sh

--- selenium_tests/myscript.sh
+++ selenium_tests/myscript.sh
@@ -1,5 +1,3 @@
-wget -q https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-dpkg -i packages-microsoft-prod.deb
 apt-get update
 apt-get install -y dotnet-sdk-8.0
 dotnet test  /var/lib/jenkins/workspace/Web_Automation/selenium_tests/bin/Debug/net8.0/selenium_tests.dll --logger html

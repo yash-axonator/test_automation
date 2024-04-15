73288fc72c5bfeb2b1cfcfe6b012b75e0b2fe678
yash-axonator <161823038+yash-axonator@users.noreply.github.com>
2024-04-15 16:25:10 +0530

    Update myscript.sh

--- selenium_tests/myscript.sh
+++ selenium_tests/myscript.sh
@@ -1,3 +1,3 @@
-echo "CDT#HRbSFVm88$8$" | sudo -S apt-get update
-echo "CDT#HRbSFVm88$8$" | sudo -S apt-get install -y dotnet-sdk-8.0
+sudo -S apt-get update
+sudo -S apt-get install -y dotnet-sdk-8.0
 sudo dotnet test  /var/lib/jenkins/workspace/Web_Automation/selenium_tests/bin/Debug/net8.0/selenium_tests.dll --logger html
